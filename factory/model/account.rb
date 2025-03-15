9952da6bae237af1b4c8ccd23c9ffde1672a59ba
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-03-16 10:16:34 +1100

    Include random generators in account and use its own .build method

--- factory/model/account.rb
+++ factory/model/account.rb
@@ -1,29 +1,16 @@
 require_relative "../../model/account"
+require_relative "../random_generators"
 
 AccountRecord = Model::Account::Record
 
 module Factory
   module Model
     class Account
+      extend RandomGenerators
+
       attr_reader :accounts
 
       class << self
-        # Account numbers are always a 16-character string comprised of digits 0-9.
-        def generate_random_account_number
-          account_number = ""
-          16.times do
-            account_number.concat(rand(0...9).to_s)
-          end
-          account_number
-        end
-
-        # For the purposes of testing, account balances are a string from 0 to 100k with 2 decimal numbers.
-        # In reality, account balances can be anything over 0.
-        # This emulates the values loaded from a CSV file or similar, and it should be converted by the Model.
-        def generate_random_balance
-          format("%.2f", rand * 100000)
-        end
-
         # Randomly generates accounts with balances.
         def generate(number_of_accounts = 1)
           new(number_of_accounts).accounts
@@ -55,20 +42,17 @@ module Factory
 
         # Generate `number_of_accounts` ensuring that account numbers are always unique.
         number_of_accounts.times do
-          account_number = 0
+          account = self.class.build
+          account_number = account[0]
+
           loop do
-            account_number = self.class.generate_random_account_number
             unless used_account_numbers.include?(account_number)
               used_account_numbers.add(account_number)
               break
             end
           end
-          @accounts.push(
-            AccountRecord.new(
-              account_number: account_number,
-              balance: self.class.generate_random_balance
-            ).to_a
-          )
+
+          accounts.push(account)
         end
       end
     end
